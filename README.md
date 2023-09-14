579f40777f9d341ae51b4d78089744e6c87126d9
laetitiamichel <laetitia.michel@devoteam.com>
2023-09-14 14:25:36 +0200

    [MAJ JS + MAJ CSS + HTML + FAVICON]

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ portfolio => mettre le lien du site
 ## CHARTE TYPO
 ![charteTypo](/asset/chartre_typo.png)
 insérer du code = ``` + langage code ```
+![typo](/asset/Yanone_kaffeesatz_typo.png)
 // pour convertir la première ligne du site manifest en JSON => converteur json
   // copier coller le code
   // pour vérifier que le json fonctionne => inspecter dans chrome puis
---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", e => {
+    console.log(e.target);
+    let el = document.querySelector("head");
+
+    // Sélectionnez le bouton dans le HTML pour activer/désactiver le mode sombre
+    const toggleDarkModeButton = document.querySelector('button');
+
+    // Ajoutez un gestionnaire d'événements pour basculer le mode sombre
+    toggleDarkModeButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode'); /* ici on fait appel à la class du CSS*/
+    });
+});
