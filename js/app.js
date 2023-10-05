c914ff8468b7aa7e366074bfb65311024783f80d
laetitiamichel <laetitia.michel@devoteam.com>
2023-10-05 14:19:02 +0200

    modified:   css/main.css 	modified:   index.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,15 @@
 document.addEventListener("DOMContentLoaded", e => {
     console.log(e.target);
-    //let el = document.querySelector("head");
+    let el = document.querySelector("head");
 
     // Sélectionnez le bouton dans le HTML pour activer/désactiver le mode sombre
-    //const toggleDarkModeButton = document.querySelector('button');
+    const toggleDarkModeButton = document.querySelector('button');
 
     // Ajoutez un gestionnaire d'événements pour basculer le mode sombre
-    /*toggleDarkModeButton.addEventListener('click', () => {
+    toggleDarkModeButton.addEventListener('click', () => {
         console.log("ok");
-        document.body.classList.toggle('dark-mode'); /* ici on fait appel à la classe darkmode du CSS
-    });*/
-
-    /*document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
-        el.addEventListener('shown.bs.tab', () => {
-            const target = el.getAttribute('data-bs-target');
-            const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`);
-            bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh();
-        });
+        document.body.classList.toggle('dark-mode'); // ici on fait appel à la classe darkmode du CSS
     });
-    $('body').scrollspy({ target: '#navbar-example' });*/
-    console.log("OK");
+
+    
 });
