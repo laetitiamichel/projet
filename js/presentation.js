3cf3d5c578a8e21ce5bca556b0085738cf89bcd7
laetitiamichel <115167618+laetitiamichel@users.noreply.github.com>
2023-10-30 12:16:13 +0100

    MAJ JS presentation

---
+++ js/presentation.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", e => {
+    console.log(e.target);
+    let el = document.querySelector("head");
+
+// SROLL ANIME POUR PRESENTATION:
+
+document.getElementById("logo-2011").addEventListener("mouseover",function(){
+    document.getElementById("image-2007");
+},false);
+    });    
