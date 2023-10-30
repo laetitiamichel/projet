624ec368b28f18cf511e7c3eb43ed7adec8c8dfb
laetitiamichel <115167618+laetitiamichel@users.noreply.github.com>
2023-10-30 17:01:31 +0100

    new file:   asset/presentation/bijoux-rue-gabrielle.jpg 	modified:   css/presentation.css 	modified:   index.html 	deleted:    js/entreprise.js 	modified:   js/presentation.js

--- js/presentation.js
+++ js/presentation.js
@@ -1,10 +1,18 @@
-document.addEventListener("DOMContentLoaded", e => {
-    console.log(e.target);
-    let el = document.querySelector("head");
-
 // SROLL ANIME POUR PRESENTATION:
+// quand le logo est affiché à droite il correspond à une image à gauche
+// si le logo change, l'image change
+let logo2007 = document.getElementById("logo-2007");
+let image2007 = document.getElementById("image-2007");
+let logo2011 = document.getElementById("logo-2011");
+let image2011 = document.getElementById("image-2011");
+let image2016 = document.getElementById("image-2016");
 
-document.getElementById("logo-2011").addEventListener("mouseover",function(){
-    document.getElementById("image-2007");
-},false);
-    });    
+logo2007.addEventListener( "onscroll", (event) =>{
+    if (logo2007 === logo2007){
+        image2007 = image2011; 
+    }
+    
+});
+/* logo2011.addEventListener( "onscrollend", (event) =>{
+    image2011 = image2016; 
+}); */
