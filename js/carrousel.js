b945cf1b178e7db86d543dc0c05668a9936e68c2
laetitiamichel <laetitia.michel@devoteam.com>
2023-12-01 09:58:54 +0100

    maj caroussel

--- js/carrousel.js
+++ js/carrousel.js
@@ -90,16 +90,21 @@
         //PROJET 1:
         let projet_1 = document.getElementById("projet_1");
         projet_1.addEventListener("click",()=>{
-            window.open("https://hackmd.io/7ktEoDKWRKe8sk--XblYnw","blank")});
+            window.open("https://hackmd.io/7ktEoDKWRKe8sk--XblYnw?view","blank")});
         //PROJET 2:
         let projet_2 = document.getElementById("projet_2");
             projet_2.addEventListener("click",()=>{
-                window.open("https://hackmd.io/f46MK54fTO2tO9Otf8E7QA","blank")});
+                window.open("https://hackmd.io/f46MK54fTO2tO9Otf8E7QA?view","blank")});
         //PROJET 3:
         let projet_3 = document.getElementById("projet_3");
         projet_3.addEventListener("click",()=>{
             window.open("https://hackmd.io/xIcM9AVmSTeCpfc3CLBqew?view","blank")});
-        })
+     
+         //PROJET 4:
+         let projet_4 = document.getElementById("projet_4");
+         projet_4.addEventListener("click",()=>{
+             window.open("https://hackmd.io/kW0dlpXmR9ucY7u-Tq1KKQ?view","blank")});
+         })
     
    
     init();
