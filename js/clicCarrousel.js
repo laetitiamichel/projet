61202a30d6f6968d1a8525c164f3d1e962cfa454
laetitiamichel <laetitia.michel@devoteam.com>
2024-02-07 17:48:00 +0100

    lien projet 5

--- js/clicCarrousel.js
+++ js/clicCarrousel.js
@@ -3,19 +3,27 @@ document.addEventListener("DOMContentLoaded", (e)=>{
         let projet_1 = document.getElementById("projet_1");
         projet_1.addEventListener("click",()=>{
             window.open("https://hackmd.io/7ktEoDKWRKe8sk--XblYnw?view","blank")});
-                    /* //PROJET 2:
-                    let projet_2 = document.getElementById("projet_2");
-                        projet_2.addEventListener("click",()=>{
-                            window.open("https://hackmd.io/f46MK54fTO2tO9Otf8E7QA?view","blank")});
-                    //PROJET 3:
-                    let projet_3 = document.getElementById("projet_3");
-                    projet_3.addEventListener("click",()=>{
-                        window.open("https://hackmd.io/xIcM9AVmSTeCpfc3CLBqew?view","blank")});
+
+        //PROJET 2:
+        let projet_2 = document.getElementById("projet_2");
+        projet_2.addEventListener("click",()=>{
+        window.open("https://hackmd.io/f46MK54fTO2tO9Otf8E7QA?view","blank")});
+
+        //PROJET 3:
+        let projet_3 = document.getElementById("projet_3");
+        projet_3.addEventListener("click",()=>{
+        window.open("https://hackmd.io/xIcM9AVmSTeCpfc3CLBqew?view","blank")});
                 
-                    //PROJET 4:
-                    let projet_4 = document.getElementById("projet_4");
-                    projet_4.addEventListener("click",()=>{
-                        window.open("https://hackmd.io/kW0dlpXmR9ucY7u-Tq1KKQ?view","blank")});
-                    }) */
+        //PROJET 4:
+        let projet_4 = document.getElementById("projet_4");
+        projet_4.addEventListener("click",()=>{
+        window.open("https://hackmd.io/kW0dlpXmR9ucY7u-Tq1KKQ?view","blank")});
+
+        //PROJET 5:
+        let projet_5 = document.getElementById("projet_5");
+        projet_5.addEventListener("click",()=>{
+        window.open("https://laetitiamichel.github.io/gmail_projet_militelo/","blank")});
+
+ });
         
-});
+
