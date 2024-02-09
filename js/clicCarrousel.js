bbf23d1a93928bf9dd3d925f2b03bc46b435b692
laetitiamichel <laetitia.michel@devoteam.com>
2024-02-09 11:51:45 +0100

    lien pour le carrousel

--- js/clicCarrousel.js
+++ js/clicCarrousel.js
@@ -12,18 +12,18 @@ document.addEventListener("DOMContentLoaded", (e)=>{
         //PROJET 3:
         let projet_3 = document.getElementById("projet_3");
         projet_3.addEventListener("click",()=>{
-        window.open("https://hackmd.io/xIcM9AVmSTeCpfc3CLBqew?view","blank")});
+        window.open("https://laetitiamichel.github.io/gmail_projet_militelo/","blank")});
                 
         //PROJET 4:
         let projet_4 = document.getElementById("projet_4");
         projet_4.addEventListener("click",()=>{
-        window.open("https://hackmd.io/6RMAYuz0RRuOdeI2_iw8mQ?view","blank")});
+        window.open("https://hackmd.io/6RMAYuz0RRuOdeI2_iw8mQ","blank")});
 
         //PROJET 5:
         let projet_5 = document.getElementById("projet_5");
         projet_5.addEventListener("click",()=>{
-        window.open("https://laetitiamichel.github.io/gmail_projet_militelo/","blank")});
-
+        window.open("https://hackmd.io/xIcM9AVmSTeCpfc3CLBqew","blank")});
+        
  });
         
 
